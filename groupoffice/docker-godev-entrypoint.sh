55df8fb151e54c253bd5f499e846dfb279cae57d
Merijn Schering <mschering@intermesh.nl>
2019-12-20 11:05:09 +0100

    only add host.docker.internal to /etc/hosts if it's not resolving

--- groupoffice/docker-godev-entrypoint.sh
+++ groupoffice/docker-godev-entrypoint.sh
@@ -4,7 +4,13 @@ set -e
 echo "Settig up host.docker.internal"
 
 # For supporting host.docker.internal on Linux. See https://github.com/docker/for-linux/issues/264
-ip -4 route list match 0/0 | awk '{print $3 " host.docker.internal"}' >> /etc/hosts
+if ! host -t A "host.docker.internal" > /dev/null
+then
+    echo "Adding host.docker.internal to /etc/hosts"
+    ip -4 route list match 0/0 | awk '{print $3 " host.docker.internal"}' >> /etc/hosts
+else
+    echo "host.docker.internal already defined"
+fi
 
 echo "Calling original with $@"
 # call original

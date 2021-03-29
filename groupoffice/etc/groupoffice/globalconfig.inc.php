db338a9568ef4141dc1c1556b2b52d8fb4543bff
Merijn Schering <mschering@intermesh.nl>
2021-03-29 16:51:47 +0200

    Moved phpmyadmin and webgrind to override example

--- groupoffice/etc/groupoffice/globalconfig.inc.php
+++ groupoffice/etc/groupoffice/globalconfig.inc.php
@@ -22,6 +22,10 @@ $config = [
         'serverclient_smtp_encryption' =>'',
         'serverclient_smtp_username' => '',
         'serverclient_smtp_password' => '',
+
+	'core' => [
+//		'license' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJob3N0bmFtZSI6IiouZ3JvdXAtb2ZmaWNlLmNvbSxob3N0LmRvY2tlci5pbnRlcm5hbCIsInZlcnNpb24iOiI2LjUiLCJsaWNlbnNlcyI6eyJncm91cG9mZmljZS1wcm8iOnsiZXhwaXJlc0F0IjpudWxsfX19.NQz07VqG2i_eRSEClrxNRIJuMu9WQD7je1u48khM8BQ'
+	]
 //'product_name' => 'My Office',
 //'custom_css_url' => '/branding/style.css',
 //'support_link' => 'https://www.intermesh.nl'

db0338873f6f9840c13f5a29580980cd230a3a01
Merijn Schering <mschering@intermesh.nl>
2025-01-24 14:36:39 +0100

    Install all composer.json projects

--- groupoffice/docker-godev-entrypoint.sh
+++ groupoffice/docker-godev-entrypoint.sh
@@ -13,11 +13,22 @@ else
     echo "host.docker.internal already defined"
 fi
 
+DIR="$(pwd)";
+
 if [ ! -d "/usr/local/share/src/www/vendor" ]; then
   echo "Running composer install for PHP"
-  cd /usr/local/share/src/www
-  composer install -o
+  cd "/usr/local/share/src/www/";
+  for line in $(find . -name composer.json -type f -not -path '*/vendor/*')
+  do
+    COMPOSER_DIR="$(dirname "${line}")";
+    echo "Composer install:" $COMPOSER_DIR;
+    cd $COMPOSER_DIR;
+    composer install -o
+     cd "/usr/local/share/src/www/";
+  done
 fi
+
+cd $DIR;
 
 echo "Calling original with $@"
 # call original
